d3986c7fbbaea57d471bed690ba84c6f8afeb4cc
kaurv <van491@usask.ca>
2024-02-28 01:19:44 -0600

    Initial setup of the game

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
 using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+
+void Setup() {
+    gameOver = false;
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
 int main() {
+    Setup();
+    while (!gameOver){
+        Draw();
+        Input();
+        Logic();
+    }
     return 0;
 }
